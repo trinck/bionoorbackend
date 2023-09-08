940c85f6aab5ca5db684cda9f8b9e8200c51e5e8
trinck moulooungui <g.mouloungui_ibiatsi@mundiapolis.ma>
2023-09-08 13:30:39 +0200

    before jwt maagement

--- src/main/java/com/bionoor/api/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/bionoor/api/security/CustomAuthenticationEntryPoint.java
@@ -17,14 +17,16 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint{
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
 	    	
+		
            response.sendRedirect("/login");
       
     }
 
     private boolean isClientRequest(HttpServletRequest request) {
-        // Implement your logic to determine if the request is from a client
-        // For example, you can check the request URL or headers
-        // Return true if it's a client request, false otherwise
+		// Implement your logic to determine if the request is from a client
+		// For example, you can check the request URL or headers
+		// Return true if it's a client request, false otherwise
+    	
     	
     	
     	return true;
